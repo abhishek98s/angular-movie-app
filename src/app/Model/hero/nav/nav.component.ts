e5811003abde3f3b6475df101eaa450fea06db2d
abhishek <abhishek_mi@yonefu.info>
2023-05-09 12:46:11 +0545

    added navigation

--- src/app/Model/hero/nav/nav.component.ts
+++ src/app/Model/hero/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
-  
+  dropdown: boolean;
 
+  constructor(public appservices: AppService) {
+    this.dropdown = this.appservices.dropdown;
+  }
+
+  showDropDown() {
+    this.appservices.showDropDown();
+    this.dropdown = this.appservices.dropdown;
+  }
 }
--- src/app/Model/top-nav/top-nav.component.ts
+++ src/app/Model/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef, HostListener, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-top-nav',
@@ -8,6 +9,16 @@ import { Component, ViewChild, ElementRef, HostListener, OnInit, Input, Output, 
 
 export class TopNavComponent {
   svg_color = '#EC7532'
+  dropdown!: boolean;
+
+  constructor(public appservices: AppService) {
+    this.dropdown = this.appservices.dropdown;
+  }
+
+  showDropDown() {
+    this.appservices.showDropDown();
+    this.dropdown = this.appservices.dropdown;
+  }
 
   changeColor() {
     this.svg_color = "#ffffff"
